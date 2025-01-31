5cfcfd261fa40130d97dcc3a86ba20858df68722
Mariataveras5 <mariagtaveras5105@gmail.com>
2025-01-31 11:24:06 +0100

    Added simple string

---
+++ simple_string.py
@@ -0,0 +1,11 @@
+s = "Hello" # strings can be defined with single and double quotes
+print(s)
+s2 = 'hello sir'
+print(s2)
+s = ('And she said: "Hello!"')
+print(s)
+print(", . '\"\"\"") # backslash to break
+s = "hello!"
+print(s[2], s[4]) # you index by position, counting from 0
+print(s[-1]) # go around to the end!
+print(s[1], s[-5])
